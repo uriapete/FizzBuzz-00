08dc45f4773fb489a90295c57eadb50ead05c4bc
uriapete <uriarteangelo@gmail.com>
2023-02-17 12:29:21 -0800

    Initialized RepWord.h and set up class skeleton

--- cpp/RepWord.h
+++ cpp/RepWord.h
@@ -1,17 +1,27 @@
+#include <string>
+#include <vector>
+
 //class for words to replace words with
 class RepWord
 {
 private:
     /* data */
 public:
-    RepWord(/* args */);
+    std::string word;
+    int number;
+
+    static std::vector<RepWord> wordList;
+
+    RepWord(std::string wor, int num);
     ~RepWord();
+
+    std::vector<int> FizzBuzz(int maxNum);
 };
 
-RepWord::RepWord(/* args */)
-{
-}
+// RepWord::RepWord(/* args */)
+// {
+// }
 
-RepWord::~RepWord()
-{
-}
+// RepWord::~RepWord()
+// {
+// }
