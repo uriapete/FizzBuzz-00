de79aa69cec421518ac2d63e508c7f18bbc93d25
uriapete <uriarteangelo@gmail.com>
2023-02-17 12:22:57 -0800

    Completed FizzBuzz.js

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,3 +1,15 @@
 function fizzbuzz(maxNum) {
-    
+    let fbArr = [];
+    for (let i = 1; i <= maxNum; i++) {
+        let element = "";
+        if (i % 3 === 0) {element+="Fizz";}
+        if (i%5 === 0){element+="Buzz";}
+        if (element!==""){fbArr.push(element);} else {fbArr.push(i);}
+    }
+    return fbArr;
 }
+
+console.log(fizzbuzz(3));
+console.log(fizzbuzz(5));
+console.log(fizzbuzz(15));
+console.log(fizzbuzz(100));
